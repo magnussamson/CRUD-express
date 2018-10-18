54e2ccadc74b57688573321c02420e113e87a052
Magnus Samson <dangdainguyen.dev@gmail.com>
2018-10-18 16:16:38 +0700

    update auth

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,5 +4,30 @@ module.exports.login = function(req, res, next) {
 };
 
 module.exports.postLogin = function(req, res, next) {
-    
-}
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var user = db.get('users').find({ email: email}).value();
+
+    if( !user) {
+        res.render('auth/login', {
+            errors: [
+                'User does not exist.'
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    if (user.password !== password) {
+        res.render('auth/login', {
+            errors: [
+                'Wrong password',
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    res.redirect('/users');
+};
