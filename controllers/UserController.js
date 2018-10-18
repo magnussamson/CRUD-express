2e25a167200f070c97a8470490d4318d49e8a294
Magnus Samson <dangdainguyen.dev@gmail.com>
2018-10-18 10:56:51 +0700

    update UserController move validate code to diff place

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -63,25 +63,6 @@ module.exports.postUpdate = function(req, res) {
 
 module.exports.postCreate = function(req, res) {
     req.body.id = shortid.generate();
-    var errors = [];
-    if (!req.body.name) {
-        errors.push('Name is required.');
-    } 
-    if (!req.body.age) {
-        errors.push('Age is required.');
-    } 
-    if (!req.body.phone) {
-        errors.push('Telephone number is required.');
-    } 
-
-    if (errors.length) {
-        res.render('users/create', {
-            errors: errors, 
-            values: req.body
-        });
-        return;
-    }
-
     db.get('users').push(req.body).write();
     res.redirect('/users');
 }
